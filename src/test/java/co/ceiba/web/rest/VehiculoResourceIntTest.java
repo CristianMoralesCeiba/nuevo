53c241b4bf85ed9ab77339a52987a6ba96813982
CristianMoralesCeiba <cristian.morales@ceiba.com.co>
2017-11-15 14:56:52 -0500

    f
    f

--- src/test/java/co/ceiba/web/rest/VehiculoResourceIntTest.java
+++ src/test/java/co/ceiba/web/rest/VehiculoResourceIntTest.java
@@ -9,6 +9,7 @@ import co.ceiba.service.dto.VehiculoDTO;
 import co.ceiba.service.mapper.VehiculoMapper;
 import co.ceiba.web.rest.errors.ExceptionTranslator;
 import co.ceiba.web.rest.testDataBuilder.VehiculoTestDataBuilder;
+import co.ceiba.web.rest.util.ErrorMessages;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -21,6 +22,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,6 +34,7 @@ import java.util.List;
 import static co.ceiba.web.rest.TestUtil.createFormattingConversionService;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasItem;
+import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -109,23 +112,21 @@ public class VehiculoResourceIntTest {
 
     @Test
     @Transactional
-    public void createVehiculoWithExistingId() throws Exception {
-    	//arrage
+    public void createVehiculoWithExistingId() throws Exception {        
         VehiculoTestDataBuilder vehiculoTestDataBuilder = new VehiculoTestDataBuilder();
-    	
-        int databaseSizeBeforeCreate = vehiculoRepository.findAll().size();
-        // Create the Vehiculo
-        VehiculoDTO vehiculoDTO = vehiculoTestDataBuilder.conID(1L).build();
+        VehiculoDTO vehiculo = vehiculoTestDataBuilder.build();
         
-        // An entity with an existing ID cannot be created, so this API call must fail
-        restVehiculoMockMvc.perform(post("/api/vehiculos")
+        MvcResult result = restVehiculoMockMvc.perform(post("/api/vehiculos")
             .contentType(TestUtil.APPLICATION_JSON_UTF8)
-            .content(TestUtil.convertObjectToJsonBytes(vehiculoDTO)))
-            .andExpect(status().isBadRequest());
+            .content(TestUtil.convertObjectToJsonBytes(vehiculo)))
+            .andExpect(status().isBadRequest()).andReturn();
 
-        // Validate the Vehiculo in the database
+        /* Validate the User in the database
         List<Vehiculo> vehiculoList = vehiculoRepository.findAll();
-        assertThat(vehiculoList).hasSize(databaseSizeBeforeCreate);
+        assertThat(vehiculoList).hasSize(databaseSizeBeforeCreate + 1);
+        Vehiculo testVehiculo = vehiculoList.get(vehiculoList.size() - 1);
+        assertThat(testVehiculo.getPlaca()).isEqualTo(vehiculo.getPlaca());*/
+        assertEquals(result.getResolvedException().getMessage(), ErrorMessages.VEHICULOS_TOPE_MOTOS);
     }
 /*
     @Test
